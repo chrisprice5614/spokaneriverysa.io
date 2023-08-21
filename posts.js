d06e4c958baf80aa9cd2f9ddb97b51b2104c416f
chrisprice5614 <33729338+chrisprice5614@users.noreply.github.com>
2023-08-21 15:35:01 -0700

    Update posts.js

--- posts.js
+++ posts.js
@@ -1,14 +1,5 @@
 ///id = YYYYMMDDX X = Number of article that day
 //Always add to the top
 var items = [
-    {
-        id: 2,
-        title: "Talent Show August 14th",
-        date: "Monday, August 14th, 2023",
-        author: "",
-        text: "Get ready to get an amazing act together to wow the YSA. Join us for the Spokane River YSA talent show Monday, August 14th, 2023",
-        img: "/media/post1/01.jpg",
-        type: "EVENTS",
-        link: "/posts/001talent_show.html"
-    },
+    
 ]
